1a23ebf954a4217aa2b1fbae9c2341d627ae0edc
Timothy Cochran <timothy.cochran@gmail.com>
2014-04-10 23:14:54 -0400

    fixing filter

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,38 +2,38 @@
 
 angular.module('sf_bikes', [])
 
-    .service('Stations', function($http) {
-        var stationsPromise = $http.get('data/stations.json').then(function(response){
+.service('Stations', function($http) {
+    var stationsPromise = $http.get('data/stations.json').then(function(response){
             return response.data;
         });
 
-        this.all = function() {
-            return stationsPromise;
-        }
+    this.all = function() {
+        return stationsPromise;
+    };
 
-        this.sf = function() {
-            return stationsPromise.then(function(stations) {
-                return stations.filter(function(station){
-                    return station.landmark == 'San Francisco';
-                });
-            })
-        }
+    this.sf = function() {
+        return stationsPromise.then(function(stations) {
+            return stations.filter(function(station){
+                return station.landmark === 'San Francisco';
+            });
+        });
+    };
 
-        this.sanJose = function() {
-            return stationsPromise.then(function(stations) {
-                return stations.filter(function(station){
-                    return station.landmark == 'San Jose';
-                });
-            })
-        }
-    })
+    this.sanJose = function() {
+        return stationsPromise.then(function(stations) {
+            return stations.filter(function(station){
+                return station.landmark === 'San Jose';
+            });
+        });
+    };
+})
 
 
 .service('Trips', function($http, Stations, $q){
 
     var tripsPromise = $http.get('data/trips.json').then(function(response){
         return response.data;
-    })
+    });
     this.all = function(filterDateString) {
 
         var filterDate = Date.parse(filterDateString);
@@ -41,30 +41,29 @@ angular.module('sf_bikes', [])
         return $q.all([tripsPromise, Stations.all()]).then(function(data) {
             var trips = data[0];
             var stations = data[1];
-            var stationsLookup = {}
+            var stationsLookup = {};
             stations.forEach(function(station) {
                 stationsLookup[station.station_id] = station;
             });
 
             var findStation = function(id){
                 return stationsLookup[id];
-            }
+            };
             var result = trips.map(function(trip){
                 trip.startStation = findStation(trip['Start Terminal']);
                 trip.endStation = findStation(trip['End Terminal']);
 
                 return trip;
-            }).filter(function(trip){ 
+            }).filter(function(trip){
                 var dateString = trip['Start Date'].split(' ')[0];
-                return trip.startStation.landmark == 'San Francisco' && trip.endStation.landmark == 'San Francisco' && dateString == filterDateString;
+                return trip.startStation.landmark === 'San Francisco' && trip.endStation.landmark === 'San Francisco' && dateString === filterDateString;
             }).map(function(trip) {
-                var ms = Date.parse(trip['Start Date'])
-                trip.duration = trip['Duration'] / 60;
+                var ms = Date.parse(trip['Start Date']);
+                trip.duration = trip.Duration / 60;
                 trip.minutes = (((ms - filterDate) /1000) / 60);
                 return trip;
             });
             return result;
         });
-    
-    }
-})
+    };
+});
